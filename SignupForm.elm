dcf9a98a01214acc5b763d0f3f1d12ff9b853b39
Richard Feldman <richard.t.feldman@gmail.com>
2015-09-06 11:35:57 -0700

    Add validation fields.

--- SignupForm.elm
+++ SignupForm.elm
@@ -20,12 +20,14 @@ import Html.Attributes exposing (id, type', for, value, class)
 view model =
     form
         [ id "signup-form" ]
-        [ h1 [] [ text "Sensational Signup Form" ]
-        , label [ for "username-field" ] [ text "username: " ]
-        , input [ id "username-field", type' "text", value model.username ] []
-        , label [ for "password" ] [ text "password: " ]
-        , input [ id "password-field", type' "password", value model.password ] []
-        , button [ class "signup-button" ] [ text "Sign Up!" ]
+        [ h1 [] [ text "Sensational Signup Form" ],
+        , label [ for "username-field" ] [ text "username: " ],
+        , input [ id "username-field", type' "text", value model.username ] [],
+        , div [ class "validation-error" ] [ text model.errors.username ],
+        , label [ for "password" ] [ text "password: " ],
+        , input [ id "password-field", type' "password", value model.password ] [],
+        , div [ class "validation-error" ] [ text model.errors.password ],
+        , div [ class "signup-button" ] [ text "Sign Up!" ]
         ]
 
 
